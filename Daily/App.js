ede24d09f84d99eadf9cb43565f8a20446796d92
sawantvidisha10 <sawantvidisha10@gmail.com>
2022-05-08 16:15:12 -0400

    main

--- Daily/App.js
+++ Daily/App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import colors from './TodoColors'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style ={{flexDirection: "row"}}>
+          <View style ={styles.divider} />
+          <Text style ={styles.title}>
+            Todo <Text style ={{fontWeight:"300", color: colors.black}}>Lists</Text>
+          </Text>
+        </View>
+      </View>
+    );
+  }  
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +25,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  divider:{
+    backgroundColor: colors.lightBlue,
+    height: 1,
+    flex: 1,
+    alignSelf:"center"
+  }
+  title:{
+    fontSize: 38,
+    fontWeight: "800",
+    color: colors.black,
+    paddingHorizontal:64
+  }
 });
