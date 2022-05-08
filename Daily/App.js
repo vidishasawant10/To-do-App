a8333b618d5c7e7e39026edb43796cb5bc430764
sawantvidisha10 <sawantvidisha10@gmail.com>
2022-05-08 17:54:48 -0400

    main

--- Daily/App.js
+++ Daily/App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import colors from './TodoColors'
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {AntDesign} from '@expo/vector-icons'
+import colors from './TodoColors';
 
 export default class App extends React.Component {
   render() {
@@ -10,8 +11,18 @@ export default class App extends React.Component {
         <View style ={{flexDirection: "row"}}>
           <View style ={styles.divider} />
           <Text style ={styles.title}>
-            Todo <Text style ={{fontWeight:"300", color: colors.black}}>Lists</Text>
+            Todo <Text style ={{fontWeight:"300", color: colors.blue}}>Lists</Text>
           </Text>
+          <View style ={styles.divider} />
+        </View>
+
+        <View style ={{marginVertical: 48}}>
+          <TouchableOpacity style = {styles.addList}>
+            <AntDesign name="plus" size ={16} color ={colors.blue}/>
+          </TouchableOpacity>
+
+          <Text style={styles.add}>Add List</Text>
+          
         </View>
       </View>
     );
@@ -29,12 +40,27 @@ const styles = StyleSheet.create({
     backgroundColor: colors.lightBlue,
     height: 1,
     flex: 1,
-    alignSelf:"center"
-  }
+    alignSelf:"center",
+    
+  },
   title:{
     fontSize: 38,
     fontWeight: "800",
     color: colors.black,
-    paddingHorizontal:64
-  }
+    paddingHorizontal:64,
+  },
+  addList:{
+    borderWidth: 2,
+    borderColor: colors.lightBlue,
+    borderRadius: 4,
+    padding:16,
+    alignItems:"center",
+    justifyContent:"center"
+  },
+  add:{
+    color:colors.blue,
+    fontWeight: "600",
+    fontSize: 14,
+    marginTop: 8
+  },
 });
