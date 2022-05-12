6fca7ea00aff543761fff8d6208d16a6a8ead0d7
sawantvidisha10 <sawantvidisha10@gmail.com>
2022-05-12 16:15:34 -0400

    Update

---
+++ Daily/tempData.js
@@ -0,0 +1,50 @@
+// Temporary Data until we connect with firebase
+
+export default tempData =[
+    {
+        name: "Plan a Trip",
+        color: "#24A6D9",
+        todos:[
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title : "Passport Check",
+                completed: true
+            },
+            {
+                title:"Reserve Hotel Room",
+                completed:true
+            },
+            {
+                title:"Pack Luggage",
+                completed: false
+            },
+
+        ]
+    },
+    {
+        name: "Grocerries",
+        color: "#24A6D9",
+        todos:[
+            {
+                title: "Milk",
+                completed: false
+            },
+            {
+                title : "Cheese",
+                completed: true
+            },
+            {
+                title:"Butter",
+                completed:true
+            },
+            {
+                title:"Green Vegetables",
+                completed: false
+            },
+
+        ]
+    },
+]
